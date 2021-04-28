25b717bb1abab12eda824c7d927a9d0a5670d07c
Mulhima <81007262+TheMulhima@users.noreply.github.com>
2021-04-28 17:47:26 +0400

    Create Settings.cs

---
+++ Change Ingame Text/Settings.cs
@@ -0,0 +1,67 @@
+using Modding;
+
+namespace ChangeIngameTextMod
+{
+    public class GlobalModSettings : ModSettings
+    {
+        public string Original1 = "";
+        public string NewWord1= "";
+
+        public string Original2 = "";
+        public string NewWord2 = "";
+
+        public string Original3 = "";
+        public string NewWord3 = "";
+
+        public string Original4 = "";
+        public string NewWord4 = "";
+
+        public string Original5 = "";
+        public string NewWord5 = "";
+
+        public string Original6 = "";
+        public string NewWord6 = "";
+
+        public string Original7 = "";
+        public string NewWord7 = "";
+
+        public string Original8 = "";
+        public string NewWord8 = "";
+
+        public string Original9 = "";
+        public string NewWord9 = "";
+
+        public string Original10 = "";
+        public string NewWord10 = "";
+
+        public string Original11 = "";
+        public string NewWord11 = "";
+
+        public string Original12 = "";
+        public string NewWord12 = "";
+
+        public string Original13 = "";
+        public string NewWord13 = "";
+
+        public string Original14 = "";
+        public string NewWord14 = "";
+
+        public string Original15 = "";
+        public string NewWord15 = "";
+
+        public string Original16 = "";
+        public string NewWord16 = "";
+
+        public string Original17 = "";
+        public string NewWord17 = "";
+
+        public string Original18 = "";
+        public string NewWord18 = "";
+
+        public string Original19 = "";
+        public string NewWord19 = "";
+
+        public string Original20 = "";
+        public string NewWord20 = "";
+    }
+}

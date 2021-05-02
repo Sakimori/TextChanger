dc1a43ae337501613c353cceb9354dd8595186e5
Mulhima <81007262+TheMulhima@users.noreply.github.com>
2021-05-02 21:39:24 +0400

    Update Settings.cs
    Made it into a dict because it makes more sense

--- Change Ingame Text/Settings.cs
+++ Change Ingame Text/Settings.cs
@@ -1,67 +1,33 @@
+using System.Collections.Generic;
 using Modding;
 
 namespace ChangeIngameTextMod
 {
     public class GlobalModSettings : ModSettings
     {
-        public string Original1 = "";
-        public string NewWord1= "";
+        public Dictionary<string, string> TextToChange = new Dictionary<string, string>()
+        {
+            {"Dummy1","Dummy_Text"},
+            {"Dummy2","Dummy_Text"},
+            {"Dummy3","Dummy_Text"},
+            {"Dummy4","Dummy_Text"},
+            {"Dummy5","Dummy_Text"},
+            {"Dummy6","Dummy_Text"},
+            {"Dummy7","Dummy_Text"},
+            {"Dummy8","Dummy_Text"},
+            {"Dummy9","Dummy_Text"},
+            {"Dummy10","Dummy_Text"},
+            {"Dummy11","Dummy_Text"},
+            {"Dummy12","Dummy_Text"},
+            {"Dummy13","Dummy_Text"},
+            {"Dummy14","Dummy_Text"},
+            {"Dummy15","Dummy_Text"},
+            {"Dummy16","Dummy_Text"},
+            {"Dummy17","Dummy_Text"},
+            {"Dummy18","Dummy_Text"},
+            {"Dummy19","Dummy_Text"},
+            {"Dummy20","Dummy_Text"},
+        };
 
-        public string Original2 = "";
-        public string NewWord2 = "";
-
-        public string Original3 = "";
-        public string NewWord3 = "";
-
-        public string Original4 = "";
-        public string NewWord4 = "";
-
-        public string Original5 = "";
-        public string NewWord5 = "";
-
-        public string Original6 = "";
-        public string NewWord6 = "";
-
-        public string Original7 = "";
-        public string NewWord7 = "";
-
-        public string Original8 = "";
-        public string NewWord8 = "";
-
-        public string Original9 = "";
-        public string NewWord9 = "";
-
-        public string Original10 = "";
-        public string NewWord10 = "";
-
-        public string Original11 = "";
-        public string NewWord11 = "";
-
-        public string Original12 = "";
-        public string NewWord12 = "";
-
-        public string Original13 = "";
-        public string NewWord13 = "";
-
-        public string Original14 = "";
-        public string NewWord14 = "";
-
-        public string Original15 = "";
-        public string NewWord15 = "";
-
-        public string Original16 = "";
-        public string NewWord16 = "";
-
-        public string Original17 = "";
-        public string NewWord17 = "";
-
-        public string Original18 = "";
-        public string NewWord18 = "";
-
-        public string Original19 = "";
-        public string NewWord19 = "";
-
-        public string Original20 = "";
-        public string NewWord20 = "";
     }
 }
